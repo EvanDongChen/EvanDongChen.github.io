9490dc3837f84d9acd6d648d49a8442018b932da
EvanDongChen <evanchen0609@gmail.com>
2024-10-26 12:33:10 -0700

    Added More Colors to Account for the 6 Sections

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,11 +5,16 @@ module.exports = {
     extend: {
       colors: {
         customdarkblue: 'rgb(61, 52, 139)', 
-        customlightblue: 'rgb(90, 86, 188)', 
+        custommediumblue: 'rgb(90, 86, 188)', 
+        customirisblue: 'rgb(151, 136, 178)', 
         customlightblue: 'rgb(118, 120, 237)', 
+        custombrown: 'rgb(183, 152, 119)',
+        customash: 'rgb(167, 144, 149)',
         customyellow: 'rgb(247, 184, 1)', 
         customorange: 'rgb(241, 135, 1)', 
         customered: 'rgb(243, 91, 4)', 
+
+        
        },
 
 
