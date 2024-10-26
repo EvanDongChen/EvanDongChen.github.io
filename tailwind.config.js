6cb12b689dc953e6726d38ad247fa2575563991e
EvanDongChen <evanchen0609@gmail.com>
2024-10-26 12:25:37 -0700

    Created a Set of Custom Colors for the Website

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        customdarkblue: 'rgb(61, 52, 139)', 
+        customlightblue: 'rgb(90, 86, 188)', 
+        customlightblue: 'rgb(118, 120, 237)', 
+        customyellow: 'rgb(247, 184, 1)', 
+        customorange: 'rgb(241, 135, 1)', 
+        customered: 'rgb(243, 91, 4)', 
+       },
+
+
+    },
   },
   plugins: [],
 }
