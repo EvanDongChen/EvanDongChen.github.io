fee11ea625cf66b329c8888bff5549bc0c4588c3
EvanDongChen <evanchen0609@gmail.com>
2024-10-27 16:57:00 -0700

    Edited the Border Colors to Color Scheme, Background Images looked Not Good

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,15 +4,15 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        customdarkblue: 'rgb(61, 52, 139)', 
-        custommediumblue: 'rgb(90, 86, 188)', 
-        customirisblue: 'rgb(151, 136, 178)', 
-        customlightblue: 'rgb(118, 120, 237)', 
-        custombrown: 'rgb(183, 152, 119)',
-        customash: 'rgb(167, 144, 149)',
-        customyellow: 'rgb(247, 184, 1)', 
-        customorange: 'rgb(241, 135, 1)', 
-        customered: 'rgb(243, 91, 4)', 
+        customdarkblue: '#3D348B',   // rgb(61, 52, 139)
+        custommediumblue: '#5A56BC', // rgb(90, 86, 188)
+        customirisblue: '#9788B2',    // rgb(151, 136, 178)
+        customlightblue: '#7678ED',   // rgb(118, 120, 237)
+        custombrown: '#B79877',       // rgb(183, 152, 119)
+        customash: '#A79095',         // rgb(167, 144, 149)
+        customyellow: '#F7B801',      // rgb(247, 184, 1)
+        customorange: '#F18701',      // rgb(241, 135, 1)
+        customered: '#F35B04'         // rgb(243, 91, 4)
 
         
        },
